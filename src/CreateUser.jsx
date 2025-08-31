b4808e66a55d1f64a8b0c4785b7d4369fd39de92
Codingwithriha <98806732+codingwithriha@users.noreply.github.com>
2025-08-31 20:28:14 +0500

    Update CreateUser.jsx

--- src/CreateUser.jsx
+++ src/CreateUser.jsx
@@ -11,7 +11,7 @@ const CreateUser = () => {
 
     const Submit =(e) =>{
         e.preventDefault();
-        axios.post("http://localhost:3001/createUser", {name, email, age})
+        axios.post("https://crud-backend-eta-eight.vercel.app/createUser", {name, email, age})
         .then(result => {
             console.log(result)
             navigate('/')
